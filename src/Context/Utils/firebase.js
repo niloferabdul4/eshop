40972e502da97c6a8c42e30861943c4641f5b9cf
niloferabdul4 <niloferabdul4@gmail.com>
2023-02-13 11:13:54 +0400

    updated trendscontainer

--- src/Context/Utils/firebase.js
+++ src/Context/Utils/firebase.js
@@ -1,24 +1,15 @@
 
-
-// Import the functions you need from the SDKs you need
-
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import {getAuth} from 'firebase/auth';
 import {getFirestore} from 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAvuUthZaQAA9rIy9cLgBjREUet6tId34U",
-  authDomain: "ecommerce-app-6b3ff.firebaseapp.com",
-  projectId: "ecommerce-app-6b3ff",
-  storageBucket: "ecommerce-app-6b3ff.appspot.com",
-  messagingSenderId: "204887453425",
-  appId: "1:204887453425:web:9b114b1fa9f8d5a4344350",
-  measurementId: "G-1EK4RXCD9K"
+  apiKey: "AIzaSyCTz2soJsizLS5lKzASqGZJHP-oorCeFzk",
+  authDomain: "ecommerce-app-fac17.firebaseapp.com",
+  projectId: "ecommerce-app-fac17",
+  storageBucket: "ecommerce-app-fac17.appspot.com",
+  messagingSenderId: "1053825972808",
+  appId: "1:1053825972808:web:c2bb2268a14d72e4f2767d"
 };
 
 // Initialize Firebase
